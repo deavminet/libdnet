472c731845965be5d1ab98a2716fc8547026db51
Tristan B. Kildaire <deavmi@localhost.localdomain>
2020-12-17 23:28:32 +0200

    `join()` now throws an error on send failure

--- source/libdnet/client.d
+++ source/libdnet/client.d
@@ -130,12 +130,20 @@ public final class DClient
 
 		/* Send the protocol data */
 		DataMessage protocolData = new DataMessage(reqRepQueue.getTag(), data);
-		bSendMessage(socket, protocolData.encode());
+		bool status = bSendMessage(socket, protocolData.encode());
 
-		/* Receive the server's response */
-		byte[] resp = reqRepQueue.dequeue().getData();
-
-		return cast(bool)resp[0];
+		/* If the send worked */
+		if(status)
+		{
+			/* Receive the server's response */
+			byte[] resp = reqRepQueue.dequeue().getData();
+			return cast(bool)resp[0];
+		}
+		/* If the send failed */
+		else
+		{
+			throw new DNetworkError("join");
+		}	
 	}
 
 	/**
