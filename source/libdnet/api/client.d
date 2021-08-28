4557a8c5502a23710ce05575b61dbc2dfd673748
Tristan B. Kildaire <deavmi@redxen.eu>
2021-08-28 11:43:17 +0200

    Added missing dependency

--- source/libdnet/api/client.d
+++ source/libdnet/api/client.d
@@ -1,1 +1,20 @@
 module libdnet.api.client;
+
+/**
+* Client
+*
+* Represents a connection to the server. This
+* contains quite a number of things such as
+* basic command support, notifications support
+* (asynchronous) allowing one to attached handlers
+* to each different asynchronous message types.
+*
+* Along with the basic command support is the ability
+* to queue tasks up and have handlers attached to them
+* for when they complete (server-side asycnrhonous support
+* is mirrored into the library)
+*/
+public final class Client
+{
+    
+}
