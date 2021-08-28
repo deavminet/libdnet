6ef30473c16e467ffe98fccd6857a5969707bad2
Tristan B. Kildaire <deavmi@redxen.eu>
2021-08-28 11:57:05 +0200

    FIx up module header too

--- cleanUp.sh
+++ cleanUp.sh
@@ -6,5 +6,9 @@ protoCodeFile=source/libdnet/protobuf/dnet.d
 # Fix up
 cat $protoCodeFile | sed -e "s/source.libdnet.protobuf./ /" > tmpFile
 
+# Fix up module header
+cat tmpFile | sed -e "s/dnet/libdnet.protobuf.dnet/" > tmpFile2
+rm tmpFile
+
 # This thing got whacky
-mv tmpFile $protoCodeFile
+mv tmpFile2 $protoCodeFile
