c9fbd2891404a9445caf28414ba7aa55a7ece5a6
Tristan B. Kildaire <deavmi@redxen.eu>
2021-09-15 15:49:21 +0200

    Scrapped old  code

--- source/libdnet/api/client.d
+++ source/libdnet/api/client.d
@@ -1,37 +1,7 @@
 module libdnet.api.client;
 
 import libdnet.protobuf.dnet;
+import std.socket;
+import tristanable.manager;
+import tristanable.queue;
 
-/**
-* Client
-*
-* Represents a connection to the server. This
-* contains quite a number of things such as
-* basic command support, notifications support
-* (asynchronous) allowing one to attached handlers
-* to each different asynchronous message types.
-*
-* Along with the basic command support is the ability
-* to queue tasks up and have handlers attached to them
-* for when they complete (server-side asycnrhonous support
-* is mirrored into the library)
-*/
-public final class Client
-{
-    public Channel createChannel(string name)
-    {
-        Channel newChannel;
-
-        /* TODO: Check if the channel exists firstly */
-        ChannelMessage d = new ChannelMessage();
-        d.name = name;
-        d.type = ChannelMessage.MessageType.CREATE;
-
-        return newChannel;
-    }
-}
-
-public final class Channel
-{
-
-}
