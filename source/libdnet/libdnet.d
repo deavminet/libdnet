7d096ce8f281ad0d4d5615b28c7b32b3cc32c883
Tristan B. Kildaire <deavmi@redxen.eu>
2021-12-27 13:11:41 +0200

    Removed uneeded app.d

---
+++ source/libdnet/libdnet.d
@@ -0,0 +1,14 @@
+/**
+* TODO
+*/
+module libdnet.libdnet;
+
+public class Client
+{
+	/**
+	* TODO: We need to re-write Tasky before we can start work on this (urgent)
+	* TODO: We need to add signal handling to tristanable (not, urgent)
+	* TODO: We need to check eventy and make sure it is fully completed and
+	* documented and working (also check if it requires any signal handling, doubt)
+	*/
+}
